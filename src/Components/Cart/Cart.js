7133b2714b1f626d9bd616e0950182cc8556abe0
Tusar <tusarahmed141@gmail.com>
2022-08-01 22:00:11 +0600

    Modal design complete

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,12 +1,56 @@
 import React from "react";
-import { BsFillCartPlusFill } from "react-icons/bs";
+import Modal from "react-modal";
+import { BsFillCartPlusFill, IoMdClose } from "react-icons/all";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+Modal.setAppElement("#root");
 
 const Cart = ({ cart }) => {
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = "#f00";
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
-    <div className="cart__btn">
-      <BsFillCartPlusFill className="cart__icon" />
-      <span className="cart__quantity">{cart.length}</span>
-    </div>
+    <>
+      <div className="cart__btn" onClick={openModal}>
+        <BsFillCartPlusFill className="cart__icon" />
+        <span className="cart__quantity">{cart.length}</span>
+      </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <button onClick={closeModal} className="close-btn">
+          <IoMdClose className="close-icon" />
+        </button>
+        <h2>Hello World!</h2>
+      </Modal>
+    </>
   );
 };
 
