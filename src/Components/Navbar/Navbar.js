a4c6ec207c2a66054fc2b00ee4404858615be5d2
Tusar <tusarahmed141@gmail.com>
2022-08-01 17:22:08 +0600

    Ready to create this project

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className="navigation">
+      <nav className='nav'>
+        <a href="#home" className='nav__logo'>Weapons</a>
+        
+        <div className="cart__btn">
+          
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
